7e4afd10d3aafd18746521ad667be339ab9ed237
dikshit <dikshitdhital5@gmail.com>
2026-03-26 19:11:39 +0545

    commited

--- frontend/src/components/Checkout.jsx
+++ frontend/src/components/Checkout.jsx
@@ -218,7 +218,7 @@ function Checkout({ cartItems, setShowCheckout, setShowCart, clearCart }) {
                       <div className="form-group">
                         <label>Phone Number *</label>
                         <input
-                          type="tel"
+                          type="number"
                           name="phone"
                           value={formData.phone}
                           onChange={(e) => setFormData({...formData, phone: e.target.value})}
